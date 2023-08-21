873dc6755a68ebdd4a9c959b0f0fc6977523ad16
Nguyễn Xuân Bách <110024359+Yukimarin@users.noreply.github.com>
2023-08-21 21:59:29 +0700

    up2

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,69 +1,82 @@
-import React from "react";
-import { LockOutlined, UserOutlined } from "@ant-design/icons";
-import { Button, Checkbox, Form, Input } from "antd";
-import { useNavigate, Link, Navigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
-const Login = () => {
+function LoginPage() {
+  const [userData, setUserData] = useState([]);
+  const [loginData, setLoginData] = useState({
+    username: "",
+    password: "",
+  });
   const navigate = useNavigate();
-  const onFinish = (values) => {
-    console.log("Received values of form: ", values);
-    navigate("/");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/users")
+      .then((response) => {
+        setUserData(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setLoginData((prevData) => ({ ...prevData, [name]: value }));
   };
+  console.log(userData);
+  console.log(loginData);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const user = userData.find(
+      (user) =>
+        user.username === loginData.username &&
+        user.password === loginData.password
+    );
+    console.log(user);
+    if (user) {
+      // Login successful, you can perform further actions here
+      console.log("Đăng nhập thành công:", user);
+      navigate("/");
+    } else {
+      // Invalid credentials
+      console.log("Sai thông tin đăng nhập");
+    }
+  };
+
   return (
-    <Form
-      name='normal_login'
-      className='login-form'
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-    >
-      <Form.Item
-        name='username'
-        rules={[
-          {
-            required: true,
-            message: "Please input your Username!",
-          },
-        ]}
-      >
-        <Input
-          prefix={<UserOutlined className='site-form-item-icon' />}
-          placeholder='Username'
-        />
-      </Form.Item>
-      <Form.Item
-        name='password'
-        rules={[
-          {
-            required: true,
-            message: "Please input your Password!",
-          },
-        ]}
-      >
-        <Input
-          prefix={<LockOutlined className='site-form-item-icon' />}
-          type='password'
-          placeholder='Password'
-        />
-      </Form.Item>
-      <Form.Item>
-        <Form.Item name='remember' valuePropName='checked' noStyle>
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <a className='login-form-forgot' href=''>
-          Forgot password
-        </a>
-      </Form.Item>
-
-      <Form.Item>
-        <Button type='primary' htmlType='submit' className='login-form-button'>
-          Log in
-        </Button>
-        Or <Link to='/register'>register now!</Link>
-      </Form.Item>
-    </Form>
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor='username'>Username</label>
+          <input
+            type='text'
+            name='username'
+            placeholder='Username'
+            value={loginData.username}
+            onChange={handleInputChange}
+          />
+        </div>
+        <br />
+        <div>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            name='password'
+            placeholder='Password'
+            value={loginData.password}
+            onChange={handleInputChange}
+          />
+        </div>
+        <br />
+        <button type='submit'>Login</button>
+        <Link to='/register'>Register</Link>
+      </form>
+    </div>
   );
-};
-export default Login;
+}
+
+export default LoginPage;
